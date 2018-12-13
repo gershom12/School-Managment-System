35f04514acc81da8f10c537c2efa99ff342ab695
Gershom Maluleke <Gershom.Maluleke@paramountgroup.com>
2018-12-13 13:46:52 +0200

    Added services for each entity

---
+++ School-Management-System/src/main/java/net/sms/service/ClassroomServiceLocal.java
@@ -0,0 +1,15 @@
+
+package net.sms.service;
+
+import net.sms.model.Classroom;
+import net.sms.model.School;
+import net.sms.persistence.BaseDAO;
+
+/**
+ *
+ * @author Gershom.Maluleke
+ */
+
+public interface ClassroomServiceLocal extends BaseDAO<Classroom>{
+    
+}
---
+++ School-Management-System/src/main/java/net/sms/service/SubjectServiceLocal.java
@@ -0,0 +1,16 @@
+
+package net.sms.service;
+
+import net.sms.model.Classroom;
+import net.sms.model.School;
+import net.sms.model.Subject;
+import net.sms.persistence.BaseDAO;
+
+/**
+ *
+ * @author Gershom.Maluleke
+ */
+
+public interface SubjectServiceLocal extends BaseDAO<Subject>{
+    
+}
